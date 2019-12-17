96cbc3e401f5525b49109426a307f44d278cb392
nmendozavd <nmendozavd@gmail.com>
2019-12-16 20:21:53 -0800

    Refactored to psuedoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,25 @@
+
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+  makeDancer.call(this, top, left, timeBetweenSteps);
 
-  var oldStep = blinkyDancer.step;
+  //so we must keep a copy of the old version of this function
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work
+  var oldStep = this.step;
+  console.log('oldStep' + oldStep);
+};
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+// this is the actual DANCE STEP
+makeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  // this.step();
+  makeDancer.prototype.step.call(this);
+  // console.log('makeDancer' + this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
 
-  return blinkyDancer;
+  this.$node.toggle();
 };
--- src/init.js
+++ src/init.js
@@ -12,9 +12,10 @@ $(document).ready(function() {
 
     /* dancerMakerFunctionName is a string which must match
      * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
+     * A new o1ject of the given type will be created and added
      * to the stage.
      */
+
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
@@ -22,11 +23,13 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
+
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+  
     $('body').append(dancer.$node);
   });
 });
